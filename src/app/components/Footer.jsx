84ab58baa0a575227229555adc81ff872d5bc024
piicharpa <neuah2515@gmail.com>
2023-07-18 10:51:35 +0700

    finish

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+export const Footer = (props) => {
+  return (
+    <div className="mt-3">
+      <p className="text-secondary text-center">
+        copyright © 2023 Chayanin Suatap 12345678
+      </p>
+    </div>
+  );
+};
